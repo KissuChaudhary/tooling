03cb4a30413f4c744cd75ff5d5cdd12954267657
KissuChaudhary <97441130+KissuChaudhary@users.noreply.github.com>
2024-10-18 12:50:00 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['data:'],
+    remotePatterns: [
+      {
+        protocol: 'data',
+        hostname: '',
+      },
+    ],
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
