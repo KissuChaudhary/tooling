509aaa0180aa27121a9379f883eb08a7a293846b
KissuChaudhary <97441130+KissuChaudhary@users.noreply.github.com>
2024-10-21 22:06:32 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,6 +77,20 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-CN8F63EXC1"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-CN8F63EXC1');
+          `}
+        </Script>
       </head>
       <body>
         <Providers>
