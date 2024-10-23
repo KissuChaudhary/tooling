916975b49e99a41cc222ec1d9a6a17b069424ec0
KissuChaudhary <97441130+KissuChaudhary@users.noreply.github.com>
2024-10-23 12:13:21 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,20 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/ai-image-generator',  
+        destination: '/tools/ai-image-generator',  
+        permanent: true,  
+      },
+      {
+        source: '/ai-text-to-speech',
+        destination: '/tools/ai-text-to-speech',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 export default nextConfig
