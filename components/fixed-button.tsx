73b6c36abeb8df9fb4722257cbbfcd2cba139176
KissuChaudhary <97441130+KissuChaudhary@users.noreply.github.com>
2025-01-29 07:19:01 +0530

    Create fixed-button.tsx

---
+++ components/fixed-button.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import type { ButtonHTMLAttributes } from "react"
+
+interface FixedButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string
+  className?: string
+}
+
+export function FixedButton({ text, className, ...props }: FixedButtonProps) {
+  return (
+    <button
+      className={cn(
+        "fixed hidden md:flex items-center gap-2 right-0 top-1/2 -translate-x-5 -translate-y-20 bg-secondary-200 text-black px-4 py-2 rounded-t-lg transform -rotate-90 origin-right border hover:bg-secondary-300 outline-none transition-colors duration-300 text-sm font-medium shadow-lg",
+        className,
+      )}
+      {...props}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        aria-hidden="true"
+        className="w-5 h-5"
+      >
+        <path
+          fillRule="evenodd"
+          d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-2.625 6c-.54 0-.828.419-.936.634a1.96 1.96 0 00-.189.866c0 .298.059.605.189.866.108.215.395.634.936.634.54 0 .828-.419.936-.634.13-.26.189-.568.189-.866 0-.298-.059-.605-.189-.866-.108-.215-.395-.634-.936-.634zm4.314.634c.108-.215.395-.634.936-.634.54 0 .828.419.936.634.13.26.189.568.189.866 0 .298-.059.605-.189.866-.108.215-.395.634-.936.634-.54 0-.828-.419-.936-.634a1.96 1.96 0 01-.189-.866c0-.298.059-.605.189-.866zm2.023 6.828a.75.75 0 10-1.06-1.06 3.75 3.75 0 01-5.304 0 .75.75 0 00-1.06 1.06 5.25 5.25 0 007.424 0z"
+          clipRule="evenodd"
+        />
+      </svg>
+      {text}
+    </button>
+  )
+}
+
